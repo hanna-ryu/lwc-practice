bd1c8ec9256a0366b2e56fc54e992f597fc7b82a
hanna <foureaf@gmail.com>
2024-07-02 16:02:42 +0900

    컨디션에 따라 컴포넌트 보이기/숨기기

---
+++ force-app/main/default/lwc/renderConditionalTemplate/renderConditionalTemplate.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class RenderConditionalTemplate extends LightningElement {
+
+    isVisible = true;
+
+    handleClick(){
+        if(this.isVisible == true){
+
+            this.isVisible = false;
+        }else{
+            this.isVisible = true;
+        }
+    }
+}
